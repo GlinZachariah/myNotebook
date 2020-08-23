f02ff79ad48aa07faa0976d6659a99d5250fa19e
glinzac <glinzac@gmail.com>
2020-08-23 16:56:21 +0530

    fixed routing and added basics

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from "./app-routing.module";
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { NgxSummernoteModule } from "ngx-summernote";
 import { HttpClientModule } from '@angular/common/http';
+import { NotebookModule } from "./notebook/notebook.module";
+import { EditorModule } from "./editor/editor.module";
+import { ViewerModule } from "./viewer/viewer.module";
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
+    AppRoutingModule,
     HttpClientModule,
-    NgxSummernoteModule
+    NgxSummernoteModule,
+    EditorModule,
+    ViewerModule,
+    NotebookModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/notebook/notebook.module.ts
+++ src/app/notebook/notebook.module.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { SectionComponent } from './section/section.component';
 import { PageComponent } from './page/page.component';
 import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
 
 
 
 @NgModule({
-  declarations: [SectionComponent, PageComponent, HomeComponent],
+  declarations: [SectionComponent, PageComponent, HomeComponent, HeaderComponent],
   imports: [
     CommonModule
   ]
