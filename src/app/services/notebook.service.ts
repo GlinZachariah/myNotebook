2dc4a1a83351227472a4ad4bea6bbb6d47efeeda
glinzac <glinzac@gmail.com>
2020-09-01 23:45:03 +0530

    splitting services

--- src/app/services/notebook.service.ts
+++ src/app/services/notebook.service.ts
@@ -14,16 +14,18 @@ export class NotebookService {
   addNewSection(value){
     return this.http.post("/api/section/addNewSection",new RequestModel("addNewSection",value));
   }
-
+        
   getSectionDetails(){
     return this.http.get("/api/section/getSectionDetails");
   }
 
   deleteSection(value){
-    return this.http.post("/api/section/deleteSection", new RequestModel("deleteSection",value));
+    return this.http.delete("/api/section/deleteSection/"+value);
   }
 
   updateSection(oldValue,newValue){
     return this.http.put("/api/section/editSection",new RequestModel("editSection",{oldName:oldValue,newName:newValue}));
   }
+
+  
 }
